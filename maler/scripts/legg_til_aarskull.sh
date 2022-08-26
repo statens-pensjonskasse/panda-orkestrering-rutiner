c0694336058f4aa6bba1c6408b74b2d5065866a2
Kieu Nguyen <kin@spk.no>
2022-08-26 16:18:28 +0200

    SPKPRODUKT-5724: Oppdaterer til kopiering av 2022.09.01

--- maler/scripts/legg_til_aarskull.sh
+++ maler/scripts/legg_til_aarskull.sh
@@ -6,12 +6,12 @@ set -o nounset
 #===========================================================
 # Beskrivelse:
 #===========================================================
-# Kopierer flettede kontofoeringsfiler for 2022.08.01 fra
+# Kopierer flettede kontofoeringsfiler for 2022.09.01 fra
 # arkivet for saa aa legge til Aarskull paa kolonne 22
 # basert paa de fire foerste verdiene i kolonne 1.
 # Deretter lagres den nye filen i arkivet under
 # fakturering/pa_fak_ba_11/manuelle_tilpasninger/legg_til_aarskull/
-# 2022.08.01/grunnlagsdata_2022-08-31_00-00-00-00
+# 2022.09.01/grunnlagsdata_2022-08-31_00-00-00-00
 #===========================================================
 
 #===========================================================
@@ -27,15 +27,15 @@ set -o nounset
 
 
 PA_FAK_BA_11_AKRIV_STI="${SPK_DATA}/panda/arkiv/fakturering/pa_fak_ba_11"
-NY_ARKIV_STI="manuelle_tilpasninger/legg_til_aarskull/2022.08.01/grunnlagsdata_2022-08-31_00-00-00-00"
+NY_ARKIV_STI="manuelle_tilpasninger/legg_til_aarskull/2022.09.01/grunnlagsdata_2022-08-31_00-00-00-00"
 GRUNNLAGSDATAMAPPE="<grunnlagsdatamappe_som_skal_kopieres>"
-GRUNNLAGSDATAMAPPE_I_ARKIV="${PA_FAK_BA_11_AKRIV_STI}/flette_feilende_medlemmer/2022.08.01/${GRUNNLAGSDATAMAPPE}"
+GRUNNLAGSDATAMAPPE_I_ARKIV="${PA_FAK_BA_11_AKRIV_STI}/flette_feilende_medlemmer/2022.09.01/${GRUNNLAGSDATAMAPPE}"
 FLETTEDE_KONTOFOERINGER="flettede_kontofoeringer.csv"
 
 
 mkdir -p ${PA_FAK_BA_11_AKRIV_STI}/${NY_ARKIV_STI}
 
-echo "`date` - Starter kopiering av flettede kontofoeringer"
+echo "`date` - Starter kopiering av flettede kontofoeringer for 2022.09.01"
 
 if [ -d ${GRUNNLAGSDATAMAPPE_I_ARKIV} ]
     then
@@ -48,7 +48,7 @@ if [ -d ${GRUNNLAGSDATAMAPPE_I_ARKIV} ]
             > ${PA_FAK_BA_11_AKRIV_STI}/${NY_ARKIV_STI}/$(basename $fil)
         done
     else
-        echo "Finner ikke filene for flettede kontoføringer 2022.08.01 i arkivet"
+        echo "Finner ikke filene for flettede kontoføringer 2022.09.01 i arkivet"
 fi
 
-echo "`date` - Kopiering er ferdig"
+echo "`date` - Kopiering av flettede kontofoeringer er ferdig"
